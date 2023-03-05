422d5361f63f02abc3cc455e650050bcf096f50f
ShuVeriDa <bashtarov@outlook.com>
2023-03-05 10:04:57 +0300

    added footer and navigation

---
+++ src/app/components/common/footer/Footer.tsx
@@ -0,0 +1,48 @@
+import {FC} from 'react';
+import styles from './Footer.module.scss';
+import {usePathname, useRouter} from "next/navigation";
+
+
+
+interface IFooterProps {
+}
+
+const navItems = [
+  {
+    icon: 'home',
+    link: '/'
+  },
+  {
+    icon: 'explore',
+    link: '/explore'
+  },
+  {
+    icon: 'place',
+    link: '/place'
+  },
+  {
+    icon: 'person_outline',
+    link: '/profile'
+  },
+]
+
+export const Footer: FC<IFooterProps> = () => {
+  const pathname = usePathname()
+  const {push} = useRouter()
+  console.log(pathname)
+  return (
+    <footer className={styles.footer}>
+      <nav>
+        {navItems.map((item) => (
+            <button key={item.icon}
+                    onClick={() => push(item.link)}
+                    className={pathname === item.link ? styles.active : ''}
+            >
+              <span className='material-icons-outlined'>{item.icon}</span>
+            </button>
+          )
+        )}
+      </nav>
+    </footer>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
+'use client'
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './assets/styles/page.module.css'
+import {Footer} from "@/app/components/common/footer/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-       sss
-      </div>
-    </main>
+    <div className={styles.container}>
+      <Footer />
+    </div>
   )
 }
